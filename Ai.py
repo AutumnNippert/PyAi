b879f7f88eb4638a486d31e57be4d72e6d7112cf
Chris <33527729+chris@users.noreply.github.com>
2018-12-01 16:59:02 -0500

    Added Stuff

---
+++ Ai.py
@@ -0,0 +1,12 @@
+import re
+import random
+while(1):
+    print("Type a word and I'll respond the correct way\n")
+    word = input("")
+    with open("corrections.txt") as openfile:
+        for line in openfile:
+            if(word)in line:
+                words = (re.split(':|=', line)[-1])
+                random_phrase = random.choice(words)
+                print (random_phrase)
+    input ("Press [ENTER] to restart...")
---
+++ AiTraining.py
@@ -0,0 +1,21 @@
+import random
+while(1):
+    a = 0
+    word = input("")
+    if(word == "quit"):
+        quit()
+    while(a == 0):
+        with open("words.txt") as word_file:
+            words = word_file.readlines()
+        random_phrase = random.choice(words)
+        with open("corrections.txt") as openfile:
+            if(random_phrase)in openfile:
+                a = 0
+            else:
+                a = 1
+    print(random_phrase)
+    print("\nIf correct, type y. If not, type n")
+    correct = input("")
+    if(correct == "y"):
+        with open("corrections.txt", "a") as myfile:
+            myfile.write(word+"="+random_phrase+"\n")
